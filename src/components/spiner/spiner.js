9c1e7a28bf2cd3fc80b5496f9db91e94bdf403be
yurii <yuriy.tim@yahoo.com>
2020-04-01 22:40:27 -0400

    work with api

---
+++ src/components/spiner/spiner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './spiner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-nzonew825j">
+      <div class="ldio-tfgh2ajtj8n">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+
+// const swapi = new SwapiService();
+// swapi.getStarShip(9).then(body => {
+//   console.log(body.name)
+// });
+// fetch('https://swapi.co/api/people/1')
+// .then((res) => {
+//   return res.json();
+// }).then((body) => {
+//   console.log(body)
+// })
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />,
+  document.getElementById('root'));
